fc42f4e08df056cde2218fcbcc7ac17a01fde7c9
Hamdi Chebbi <hamdi.chebbi@outlook.fr>
2018-07-05 16:23:12 +0100

    Delete System

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../shared/employee.service';
 import { Employee } from '../shared/employee.model';
+import { ToastContainerDirective, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,7 +11,7 @@ import { Employee } from '../shared/employee.model';
 export class EmployeeListComponent implements OnInit {
   employeeList: Employee[];
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService, private toastr: ToastrService) { }
 
   ngOnInit() {
     let x = this.employeeService.getData();
@@ -29,4 +30,10 @@ export class EmployeeListComponent implements OnInit {
     this.employeeService.selectedEmployee = Object.assign({}, emp);
   }
 
+  onDelete(key: string) {
+    if (confirm('Are you sure to delete this item ? ') == true) {
+      this.employeeService.deleteEmployee(key);
+      this.toastr.error('Deleted Succefully', 'Employee Register');
+    }
+  }
 }
